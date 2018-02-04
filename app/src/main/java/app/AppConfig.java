44331dc11363b80f380b661dd58da069e5b947d9
Tahakagit <francescodar@gmail.com>
2018-02-04 11:21:56 +0100

    login funziona

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by franc on 03/02/2018.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.155/CaffeDuomoServerSide/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.155/CaffeDuomoServerSide/register.php";
+}
